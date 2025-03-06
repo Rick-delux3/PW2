ee78c4deec982832f0054abb077f51ba12069be8
Rick-delu3 <henriquerodriguesnves4@gmail.com>
2025-03-06 18:25:32 -0300

    Bootstrap

---
+++ 4_include_require/index.php
@@ -0,0 +1,12 @@
+<?php include './header.php'; ?>
+
+<div class="container-fluid bg-custom">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center align-items-center flex-column" style="min-height: 88vh">
+            <h1>As sete Maravilhas do mundo</h1>
+            <h3>Conheça as 7 maravilhas do mundo e seus respectivos países</h3>
+            <button class="btn btn-primary">Saiba mais</button>
+        </div>
+    </div>
+</div>
+<?php include './footer.php'; ?>
---
+++ 4_include_require/india.php
@@ -0,0 +1,3 @@
+<?php include './header.php'; ?>
+<h1>india</h1>
+<?php include './footer.php'; ?>
