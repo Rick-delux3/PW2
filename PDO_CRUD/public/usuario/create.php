4876ce0cc24f004cc5c87848c289c8c57e34f9d8
Rick-delux3 <henriquerodriguesneves4@gmail.com>
2025-04-22 16:31:07 -0300

    BD com PHP

---
+++ PDO_CRUD/public/usuario/create.php
@@ -0,0 +1,26 @@
+<?php
+    include '../../config/connection.php';
+?>
+
+<form action="create.php" method="POST">
+    <label for="username">Nome de Usuário</label>
+    <input type="text" name="username" id="username">;
+
+    <label for="password">Senha:</label>
+    <input type="password" name="password" id="password">
+
+    <button type="submit">Cadastrar:</button>
+</form>
+
+<?php
+    
+    $username = isset($_POST['username']) ? $_POST['username'] : exit() ;
+    $password = isset($_POST['password']) ? $_POST['password'] : exit() ;
+
+    //statement
+    $stmt = $pdo->prepare('INSERT INTO usuario (id, username, password) VALUES (:id, :username, :password');
+    $stmt->bindParam(':id', 0);
+    $stmt->binParam(':username', $username);
+    $stmt->binParam(':password', $password);
+    $stmt->execute();
+?>
