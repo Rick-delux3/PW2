1857dd0fae26ddc38ce4bb16889d45ee4f3cdd40
Rick-delux3 <henriquerodriguesneves@gmail.com>
2025-05-20 16:23:27 -0300

    PDO_CRUD

--- 2_bimestre/PDO_CRUD/public/usuario/read.php
+++ 2_bimestre/PDO_CRUD/public/usuario/read.php
@@ -1,18 +1,30 @@
+<?php include '../../includes/header.php'; ?>
+
 <?php 
 include '../../config/connection.php';
 
 $stmt = $pdo->query('SELECT * FROM usuario');
 $usuarios = $stmt->fetchAll();
 
+if( isset($_GET['id']) ){
+    $id = $_GET['id'];
+    echo "O id $id foi removido!";
+}
+
 //echo "<pre>";
 //echo var_dump($usuarios);
 
 ?>
 
-<?phpforeach ($usuarios as $indice => $user) { ?>
+<?php foreach ($usuarios as $indice => $user) { ?>
     
     <p>
         <strong>Nome da Heroina:</strong><?php echo $user['username']; ?>
+        <br>
+        <a href="http://localhost/PW2/2_bimestre/PDO_CRUD/public/usuario/delete.php?id=<?php echo $user['id']; ?>">Remover</a>
+                                                                             
+        <a href="">Editar</a>
+        <hr>
     </p>
     
     
